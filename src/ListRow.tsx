ee95cabfe325c16f9777469c13d4dc7797cc4670
Phuoc Do <dnprock@gmail.com>
2021-09-22 15:32:29 -0700

    Reverse order list in mobile view for buy side to match sell side: price, size, total.

--- src/ListRow.tsx
+++ src/ListRow.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ListRowProps } from "./interfaces";
-import { formatNumber, formatPrice } from './utilities';
+import { formatNumber, formatPrice, isMobileView } from './utilities';
 
 const ListRow = React.memo((props: ListRowProps) => {
   const divTotal = <div key={'price-total-' + props.index} className='order-col-1 order-total'>{formatNumber(props.total)}</div>
@@ -9,13 +9,23 @@ const ListRow = React.memo((props: ListRowProps) => {
     className='order-col-2 order-price'
     style={{color: props.color}} >{formatPrice(props.price)}</div>
   if (props.listType === 'buy') {
-    return (
-      <div key={'price-row-' + props.index} className='price-row'>
-        {divTotal}
-        {divSize}
-        {divPrice}
-      </div>
-    )
+    if (isMobileView()) {
+      return (
+        <div key={'price-row-' + props.index} className='price-row'>
+          {divPrice}
+          {divSize}
+          {divTotal}
+        </div>
+      )
+    } else {
+      return (
+        <div key={'price-row-' + props.index} className='price-row'>
+          {divTotal}
+          {divSize}
+          {divPrice}
+        </div>
+      )
+    }
   } else {
     return (
       <div key={'price-row-' + props.index} className='price-row'>
